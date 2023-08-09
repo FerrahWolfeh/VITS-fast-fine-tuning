9ab3b9ae39084f45436ecce1740f94dc4fe5a025
Ferrah Aiko Wolf <ferrahwolfeh@proton.me>
2023-08-09 08:24:35 -0300

    add cleanup script

---
+++ cleanup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+rm -r pretrained_models
+# download data for fine-tuning
+rm sampled_audio4ft_v2.zip
+# create necessary directories
+rm -r video_data
+rm -r raw_audio
+rm -r denoised_audio
+rm -r custom_character_voice
+rm -r segmented_character_voice
+
