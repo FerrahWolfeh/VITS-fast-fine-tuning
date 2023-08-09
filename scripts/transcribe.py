1aba7869274b16729c832705639c6126e7667187
Ferrah Aiko Wolf <ferrahwolfeh@proton.me>
2023-08-09 07:54:51 -0300

    fix path handling and other stuff

--- scripts/transcribe.py
+++ scripts/transcribe.py
@@ -3,21 +3,25 @@ import torchaudio
 import argparse
 import os
 
-target_sr = 16000
-
 
 def split_text_and_write_metadata(
-    audio_file_path: str, data_list, parent_path: str, iteration: int, char_name: str, resample: int
+    audio_file_path: str,
+    data_list,
+    parent_path: str,
+    iteration: int,
+    char_name: str,
+    resample: int,
 ):
-    metadata_file_path = parent_path + "/metadata.csv"
+    metadata_file_path = os.path.join(parent_path, "metadata.csv")
+    wav_path = os.path.join(parent_path, "wavs")
 
-    if not os.path.exists(parent_path + "/wavs/"):
-        os.makedirs(parent_path + "/wavs/")
+    if not os.path.exists(wav_path):
+        os.makedirs(wav_path)
 
     with open(metadata_file_path, "a") as metadata_file:
         for i, obj in enumerate(data_list):
             start_time = obj["start"]
-            end_time = obj["end"] + 0.2
+            end_time = obj["end"] + 0.3
 
             duration = end_time - start_time
 
@@ -32,12 +36,11 @@ def split_text_and_write_metadata(
             else:
                 print(f"{text} | {duration:.2f}s")
 
-
             metadata_line = f"{char_name}_{iteration}_{i}|{text}\n"
             metadata_file.write(metadata_line)
             output_file_name = f"{char_name}_{iteration}_{i}.wav"
             # Update the output directory as needed
-            output_file_path = parent_path + f"/wavs/{output_file_name}"
+            output_file_path = os.path.join(wav_path, output_file_name)
 
             wav, sr = torchaudio.load(  # type: ignore
                 audio_file_path,
@@ -60,10 +63,13 @@ def split_text_and_write_metadata(
                 )
 
             if wav.shape[1] / target_sr > 10:
-                wav = wav[:, int(float(start_time * target_sr)) : int(float(end_time * target_sr))]
+                wav = wav[
+                    :,
+                    int(float(start_time * target_sr)) : int(
+                        float(end_time * target_sr)
+                    ),
+                ]
 
-
-            
             torchaudio.save(  # type: ignore
                 output_file_path,
                 wav,
@@ -78,10 +84,25 @@ def split_text_and_write_metadata(
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input", default="./")
+    parser.add_argument("-i", "--input_dir", default="./")
     parser.add_argument("--enable-demucs", type=bool, default=False)
-    parser.add_argument("--resample", type=int, default=None)
+    parser.add_argument(
+        "-s",
+        "--resample",
+        type=int,
+        default=None,
+        help="Specify the final sample rate of the transcribed files",
+    )
     parser.add_argument("--language", type=str, default=None)
+    parser.add_argument(
+        "--model_dir",
+        type=str,
+        default=None,
+        help="Where to download the Whisper models",
+    )
+    parser.add_argument(
+        "--model", "-m", type=str, default="medium", help="Which Whisper model to use"
+    )
     args = parser.parse_args()
 
     parent_dir = args.input
@@ -89,22 +110,28 @@ if __name__ == "__main__":
     speaker_names = list(os.walk(parent_dir))[0][1]
     total_files = sum([len(files) for _, _, files in os.walk(parent_dir)])
 
-    model = stable_whisper.load_model(
-        "medium", download_root="/mnt/Mass/Resources/Whisper"
-    )
+    model = stable_whisper.load_model(args.model, download_root=args.model_dir)
     # this modified model run just like the original model but accepts additional arguments
 
     processed_files = 0
     for speaker in speaker_names:
-        print(f"Transcribing audios from {speaker}")
-        spk_dir = parent_dir + "/" + speaker
+        spk_dir = os.path.join(parent_dir, speaker)
 
-        if os.path.exists(spk_dir + "/metadata.csv"):
-            print(f"Skipping {speaker}")
+        file_list = len(
+            [f for f in os.listdir(spk_dir) if os.path.isfile(os.path.join(spk_dir, f))]
+        )
+
+        meta_path = os.path.join(spk_dir, "metadata.csv")
+
+        if os.path.exists(meta_path):
+            print(f"Meta file found, skipping {speaker}")
+            total_files -= file_list
             continue
 
+        print(f"Transcribing audios from {speaker}")
+
         for i, wavfile in enumerate(list(os.walk(spk_dir))[0][2]):
-            original_mp3 = parent_dir + "/" + speaker + "/" + wavfile
+            original_mp3 = os.path.join(spk_dir, wavfile)
 
             print(f"Working on file: {original_mp3}")
 
@@ -114,7 +141,10 @@ if __name__ == "__main__":
                 decode_opts.update({"language": args.language})
 
             result = model.transcribe(
-                original_mp3, vad=False, demucs=args.enable_demucs, decode_options=decode_opts
+                original_mp3,
+                vad=False,
+                demucs=args.enable_demucs,
+                decode_options=decode_opts,
             )
 
             (
@@ -128,7 +158,9 @@ if __name__ == "__main__":
 
             flist = result.to_dict()["segments"]  # type: ignore
 
-            split_text_and_write_metadata(original_mp3, flist, spk_dir, i, speaker, args.resample)
+            split_text_and_write_metadata(
+                original_mp3, flist, spk_dir, i, speaker, args.resample
+            )
 
             processed_files += 1
             print(f"Processed: {processed_files}/{total_files}")
